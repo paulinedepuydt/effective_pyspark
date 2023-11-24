6e09822a461b6d591ab38eacad0bfb299371bbfb
Pauline Depuydt <pauline.depuydt@axxes.com>
2023-11-24 13:58:00 +0000

    dates exercise

--- exercises/c_labellers/dates.py
+++ exercises/c_labellers/dates.py
@@ -1,9 +1,8 @@
 import datetime
 from pyspark.sql import DataFrame
+from pyspark.sql.functions import udf, col
 import holidays
-
-
-datetime.date(2015, 1, 1) in holidays.BE()  # True
+from pyspark.sql.types import BooleanType
 
 
 def is_belgian_holiday(date: datetime.date) -> bool:
@@ -14,7 +13,14 @@ def is_belgian_holiday(date: datetime.date) -> bool:
 def label_weekend(frame: DataFrame, colname: str = "date", new_colname: str = "is_weekend") -> DataFrame:
     """Adds a column indicating whether or not the attribute `colname`
     in the corresponding row is a weekend day."""
-    pass
+    def is_weekend(my_date: datetime.date):
+        weekday = my_date.weekday()
+        return weekday >= 5
+
+    udf_weekend = udf(lambda z: is_weekend(z), BooleanType())
+    frame = frame.withColumn(new_colname, udf_weekend(col(colname)))
+    return frame
+
 
 
 def label_holidays(
